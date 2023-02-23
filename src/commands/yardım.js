eb8f94b1cfac5cecf8378214780e65618ef68e81
Wully <91089226+akincibaran@users.noreply.github.com>
2023-02-23 13:46:36 +0300

    Yardım js yapıldı
    Yardım js yapıldı

---
+++ src/commands/yardım.js
@@ -0,0 +1,43 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+const { EmbedBuilder } = require("discord.js");
+
+const config = require("../config.js");
+
+module.exports = {
+
+  data: new SlashCommandBuilder()
+
+    .setName("yardım")
+
+    .setDescription("Botun yardım menüsünü gösterir."),
+
+  run: async (client, interaction) => {
+
+    const embed = new EmbedBuilder()
+
+      .setTitle("Alvi | Yardım Menüsü")
+
+      .setThumbnail(client.user.avatarURL())
+
+      .setColor("Blue")
+
+      .setAuthor({
+
+        name: "Alvi",
+
+        iconURL: client.user.avatarURL({ dynamic: true }),
+
+      })
+
+      .setDescription(
+
+        `**/yardım** \`=\` Yardım menüsünü gösterir.\n**/deprem** \`=\` Deprem Bilgi sistemi komutları.\n**/istatistik** \`=\` Bot istatistigi.\n**/ping** \`=\` Botun gecikme süresini gösterir.`)
+
+      );
+
+    interaction.reply({ embeds: [embed] });
+
+  },
+
+};
